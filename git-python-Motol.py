b910d41a5289475def007f641c0e8b1e809ca32b
MotolMarianne <motolmarianneee@gmail.com>
2022-03-29 16:46:36 +0800

    My Educational Background added

---
+++ git-python-Motol.py
@@ -0,0 +1,19 @@
+def personal_details():
+    name = "Marianne Ebarola Motol"
+    address = "Pangi Gasan Marinduque"
+    birthday = "September 27, 2001"
+    print(name)
+    print(address)
+    print(birthday)
+ 
+personal_details()
+
+def educational_bg():
+    college = "Marinduque State College"
+    course = "Bachelor of Information Technology"
+    year = "2E"
+    print(college)
+    print(course)
+    print(year)
+ 
+educational_bg()
